5d67de1e2b80877c679a6ad424970aa00e177eec
Mitch Oz <divine_comedian@protonmail.com>
2023-06-19 08:27:22 -0600

    more tests

--- test/Deposit.t.sol
+++ test/Deposit.t.sol
@@ -1,1 +1,113 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import './proStakingTest.t.sol';
+
+contract tesDeposit is ProStakingTest {
+
+    function setUp() public override {
+        super.setUp();
+        console.log("upgrade price is", upgradePrice);
+    }
+
+    function testDeposit(uint256 tokenId) public {
+        vm.startPrank(stakerOne);
+        vm.expectEmit(true,true,true,true, address(givToken));
+        emit Approval(address(stakerOne), address(proStaking), upgradePrice);
+
+        vm.expectEmit(true,true,true,true, address(givToken));
+        emit Transfer(address(stakerOne), address(proStaking), upgradePrice);
+
+        vm.expectEmit(true,true,true,true, address(proStaking));
+        emit TransferSingle(address(stakerOne), address(0), address(stakerOne), tokenId, 1);
+
+        vm.expectEmit(true, true, true, true, address(proStaking));
+        emit Deposit(address(stakerOne), tokenId, upgradePrice);
+
+        vm.expectEmit(true,true,true,true, address(proStaking));
+        emit AddStake(address(stakerOne), tokenId);
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(tokenId);
+
+        assertEq(proStaking.totalSupply(tokenId), 1);
+
+        givToken.approve(address(proStaking), upgradePrice);
+        // single user cannot have more than one of a given tokenId
+        vm.expectRevert(ProStaking.RecipientAlreadyHasDeposit.selector);
+        proStaking.depositAndMint(tokenId);
+
+        // multiple users can have 1 token of the same tokenId
+        vm.stopPrank();
+        vm.startPrank(stakerTwo);
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(tokenId);
+
+        assertEq(proStaking.totalSupply(tokenId), 2);
+    }
+
+    function testDepositMultiple() public {
+        uint256 idOne = 1;
+        uint256 idTwo = 2;
+        uint256 idThree = 3;
+        uint256 idFour = 4;
+        uint256 idFive = 5;
+
+        vm.startPrank(stakerOne);
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(idOne);
+
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(idTwo);
+
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(idThree);
+         vm.stopPrank();
+
+        assertEq(proStaking.totalSupply(idOne), 1);
+        assertEq(proStaking.totalSupply(idTwo), 1);
+        assertEq(proStaking.totalSupply(idThree), 1);
+
+        vm.startPrank(stakerTwo);
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(idFour);
+
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(idFive);
+
+        assertEq(proStaking.totalSupply(idFour), 1);
+        assertEq(proStaking.totalSupply(idFive), 1);
+
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(idOne);
+
+        assertEq(proStaking.totalSupply(idOne), 2);
+    }
+
+    function testFailDepositNoBalance() public {
+
+        vm.startPrank(stakerWithNoTokens);
+        givToken.approve(address(proStaking), upgradePrice);
+        proStaking.depositAndMint(1);
+        vm.stopPrank();
+        // fix this to continue testing after 1st fail;
+        vm.prank(omniBridge);
+        givToken.mint(stakerWithNoTokens, 0.5 ether);
+
+        vm.startPrank(stakerWithNoTokens);
+        proStaking.depositAndMint(1);        
+    }
+
+    function testUpgradePrice(uint256 amount) public {
+        vm.prank(deployer);
+        proStaking.setPrice(amount);
+
+        vm.prank(omniBridge);
+        givToken.mint(stakerOne, amount);
+
+        vm.startPrank(stakerOne);
+        givToken.approve(address(proStaking), amount);
+        proStaking.depositAndMint(1);
+    }
+
+
+}
--- test/proStakingTest.t.sol
+++ test/proStakingTest.t.sol
@@ -7,13 +7,15 @@ import '@openzeppelin/contracts/proxy/transparent/ProxyAdmin.sol';
 import 'forge-std/Test.sol';
 import 'forge-std/console.sol';
 import '@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol';
-import "./interfaces/IERC20Bridged.sol";
+import './interfaces/IERC20Bridged.sol';
 
 contract ProStakingTest is Test {
     using SafeERC20 for IERC20;
-   
+    uint256 public constant MAX_GIV_BALANCE = 10 ** 28; // 10 Billion, Total minted giv is 1B at the moment
 
-        address omniBridge = 0xf6A78083ca3e2a662D6dd1703c939c8aCE2e268d;
+
+
+    address omniBridge = 0xf6A78083ca3e2a662D6dd1703c939c8aCE2e268d;
     address givTokenAddress = 0x4f4F9b8D5B4d0Dc10506e5551B0513B61fD59e75;
     ProStaking implementation;
     ProStaking proStaking;
@@ -21,26 +23,53 @@ contract ProStakingTest is Test {
     TransparentUpgradeableProxy proxy;
     IERC20Bridged givToken;
     uint256 upgradePrice = 1 ether;
-    address givethMultisig = address(0);
-    address sender = address(1);
+    address deployer = address(1);
+    address givethMultisig = address(2);
+    address stakerOne = address(3);
+    address stakerTwo = address(4);
+    address stakerWithNoTokens = address(5);
+
+    event Deposit(address indexed account, uint256 indexed tokenId, uint256 amount);
+    event Withdraw(address indexed account, uint256 indexed tokenId, uint256 amount);
+    event Slash(address indexed account, uint256 indexed tokenId, uint256 amount);
+    event AddStake(address indexed account, uint256 tokenId);
+    event RemoveStake(address indexed account, uint256 tokenId);
+    event TransferDeposit(address indexed from, address indexed to, uint256 indexed tokenId, uint256 amount);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+    event Transfer(address indexed from, address indexed to, uint256 amount);
+    event TransferSingle(
+        address indexed _operator, address indexed _from, address indexed _to, uint256 _id, uint256 _value
+    );
 
     constructor() {
-         uint256 forkId = 
-            vm.createFork('https://rpc.gnosis.gateway.fm');
+        uint256 forkId = vm.createFork('https://rpc.gnosis.gateway.fm');
         vm.selectFork(forkId);
     }
 
-    function setUp() public {
-        givToken =  IERC20Bridged(address(givTokenAddress));
+    function setUp() public virtual {
+        vm.deal(deployer, 100 ether);
+        vm.startPrank(deployer);
+        givToken = IERC20Bridged(address(givTokenAddress));
         proxyAdmin = new ProxyAdmin();
         implementation = new ProStaking();
         proxy =
         new TransparentUpgradeableProxy(address(implementation), address(proxyAdmin), abi.encodeWithSelector(ProStaking(proStaking).initialize.selector, givToken, upgradePrice, givethMultisig ));
         proStaking = ProStaking(address(proxy));
-        
-        vm.prank(omniBridge);
-            givToken.mint(sender, 100 ether);
+        vm.stopPrank();
 
+        vm.startPrank(omniBridge);
+        givToken.mint(stakerOne, 100 ether);
+        givToken.mint(stakerTwo, 100 ether);
+        vm.stopPrank();
+
+        vm.label(deployer, 'deployer');
+        vm.label(stakerOne, 'stakerOne');
+        vm.label(stakerTwo, 'stakerTwo');
+        vm.label(address(givToken), 'givToken');
+        vm.label(address(proStaking), 'proStaking');
+        vm.label(address(proxyAdmin), 'proxyAdmin');
+        vm.label(address(givethMultisig), 'givethMultisig');
+        vm.label(address(implementation), 'proStakingImplementation');
 
     }
 }
